ed02529995539b876d3d0a3f493a6f68e59d9637
Greg Roach <greg@subaqua.co.uk>
2024-02-17 15:48:57 +0000

    Extend PHP version support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Provides user-land PHP polyfills for the [`mb_trim`, `mb_ltrim`, and `mb_rtrim` functions added in PHP 8.4](https://php.watch/versions/8.4/mb_trim-mb_ltrim-mb_rtrim).
 
-Requires PHP 8,1, 8.2 or PHP 8.3 with `mbstring` extension. Not supported on PHP 8.4 because these functions are natively implemented in PHP 8.4.
+Requires PHP 8.0 or higher, with `mbstring` extension. The package is a no-op on PHP 8.4 and higher because these functions are natively implemented.
 
 ## Installation
 
@@ -19,4 +19,3 @@ Simply use the `mb_trim`, `mb_ltrim`, and `mb_rtrim` functions as if they were d
 ## Contributions
 
 Contributions are welcome either as a GitHub issue or a PR to this repo.
-
--- src/mb_trim.php
+++ src/mb_trim.php
@@ -1,130 +1,133 @@
 <?php
 
-/**
- * Multi-byte safely strip white-spaces (or other characters) from the beginning and end of a string.
- *
- * @param string $string The string that will be trimmed.
- * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
- * @param string|null $encoding The encoding parameter is the character encoding.
- *
- * @return string The trimmed string.
- */
-function mb_trim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
-    // On supported versions, use a pre-calculated regex for performance.
-    if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
-        return preg_replace("/^[\s\0]+|[\s\0]+$/uD", '', $string);
+// These functions are included in PHP 8.4
+if (PHP_VERSION_ID < 80400) {
+    /**
+     * Multi-byte safely strip white-spaces (or other characters) from the beginning and end of a string.
+     *
+     * @param string $string The string that will be trimmed.
+     * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
+     * @param string|null $encoding The encoding parameter is the character encoding.
+     *
+     * @return string The trimmed string.
+     */
+    function mb_trim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
+        // On supported versions, use a pre-calculated regex for performance.
+        if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
+            return preg_replace("/^[\s\0]+|[\s\0]+$/uD", '', $string);
+        }
+
+        try {
+            @mb_check_encoding('', $encoding);
+        } catch (ValueError $e) {
+            throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
+        }
+
+        if ($characters === "") {
+            return $string;
+        }
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $string = mb_convert_encoding($string, "UTF-8", $encoding);
+            $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
+        }
+
+        $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
+        $regexClass = implode('', $charMap);
+        $regex = "/^[" . $regexClass . "]+|[" . $regexClass . "]+$/uD";
+
+        $return = preg_replace($regex, '', $string);
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $return = mb_convert_encoding($return, $encoding, "UTF-8");
+        }
+
+        return $return;
     }
 
-    try {
-        @mb_check_encoding('', $encoding);
-    } catch (ValueError $e) {
-        throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
+    /**
+     * Multi-byte safely strip white-spaces (or other characters) from the beginning of a string.
+     *
+     * @param string $string The string that will be trimmed.
+     * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
+     * @param string|null $encoding The encoding parameter is the character encoding.
+     *
+     * @return string The trimmed string.
+     */
+    function mb_ltrim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
+        // On supported versions, use a pre-calculated regex for performance.
+        if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
+            return preg_replace("/^[\s\0]+/u", '', $string);
+        }
+
+        try {
+            @mb_check_encoding('', $encoding);
+        } catch (ValueError $e) {
+            throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
+        }
+
+        if ($characters === "") {
+            return $string;
+        }
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $string = mb_convert_encoding($string, "UTF-8", $encoding);
+            $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
+        }
+
+        $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
+        $regexClass = implode('', $charMap);
+        $regex = "/^[" . $regexClass . "]+/u";
+
+        $return = preg_replace($regex, '', $string);
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $return = mb_convert_encoding($return, $encoding, "UTF-8");
+        }
+
+        return $return;
     }
 
-    if ($characters === "") {
-        return $string;
+    /**
+     * Multi-byte safely strip white-spaces (or other characters) from the end of a string.
+     *
+     * @param string $string The string that will be trimmed.
+     * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
+     * @param string|null $encoding The encoding parameter is the character encoding.
+     *
+     * @return string The trimmed string.
+     */
+    function mb_rtrim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
+        // On supported versions, use a pre-calculated regex for performance.
+        if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
+            return preg_replace("/[\s\0]+$/uD", '', $string);
+        }
+
+        try {
+            @mb_check_encoding('', $encoding);
+        } catch (ValueError $e) {
+            throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
+        }
+
+        if ($characters === "") {
+            return $string;
+        }
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $string = mb_convert_encoding($string, "UTF-8", $encoding);
+            $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
+        }
+
+        $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
+        $regexClass = implode('', $charMap);
+        $regex = "/[" . $regexClass . "]+$/uD";
+
+        $return = preg_replace($regex, '', $string);
+
+        if ($encoding !== null && $encoding !== 'UTF-8') {
+            $return = mb_convert_encoding($return, $encoding, "UTF-8");
+        }
+
+        return $return;
     }
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $string = mb_convert_encoding($string, "UTF-8", $encoding);
-        $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
-    }
-
-    $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
-    $regexClass = implode('', $charMap);
-    $regex = "/^[" . $regexClass . "]+|[" . $regexClass . "]+$/uD";
-
-    $return = preg_replace($regex, '', $string);
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $return = mb_convert_encoding($return, $encoding, "UTF-8");
-    }
-
-    return $return;
-}
-
-/**
- * Multi-byte safely strip white-spaces (or other characters) from the beginning of a string.
- *
- * @param string $string The string that will be trimmed.
- * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
- * @param string|null $encoding The encoding parameter is the character encoding.
- *
- * @return string The trimmed string.
- */
-function mb_ltrim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
-    // On supported versions, use a pre-calculated regex for performance.
-    if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
-        return preg_replace("/^[\s\0]+/u", '', $string);
-    }
-
-    try {
-        @mb_check_encoding('', $encoding);
-    } catch (ValueError $e) {
-        throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
-    }
-
-    if ($characters === "") {
-        return $string;
-    }
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $string = mb_convert_encoding($string, "UTF-8", $encoding);
-        $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
-    }
-
-    $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
-    $regexClass = implode('', $charMap);
-    $regex = "/^[" . $regexClass . "]+/u";
-
-    $return = preg_replace($regex, '', $string);
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $return = mb_convert_encoding($return, $encoding, "UTF-8");
-    }
-
-    return $return;
-}
-
-/**
- * Multi-byte safely strip white-spaces (or other characters) from the end of a string.
- *
- * @param string $string The string that will be trimmed.
- * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.
- * @param string|null $encoding The encoding parameter is the character encoding.
- *
- * @return string The trimmed string.
- */
-function mb_rtrim(string $string, string $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", ?string $encoding = null): string {
-    // On supported versions, use a pre-calculated regex for performance.
-    if (PHP_VERSION_ID >= 80200 && ($encoding === null || $encoding === "UTF-8") && $characters === " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}") {
-        return preg_replace("/[\s\0]+$/uD", '', $string);
-    }
-
-    try {
-        @mb_check_encoding('', $encoding);
-    } catch (ValueError $e) {
-        throw new ValueError(sprintf('%s(): Argument #3 ($encoding) must be a valid encoding, "%s" given', __FUNCTION__, $encoding));
-    }
-
-    if ($characters === "") {
-        return $string;
-    }
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $string = mb_convert_encoding($string, "UTF-8", $encoding);
-        $characters = mb_convert_encoding($characters, "UTF-8", $encoding);
-    }
-
-    $charMap = array_map(static fn(string $char): string => preg_quote($char, '/'), mb_str_split($characters));
-    $regexClass = implode('', $charMap);
-    $regex = "/[" . $regexClass . "]+$/uD";
-
-    $return = preg_replace($regex, '', $string);
-
-    if ($encoding !== null && $encoding !== 'UTF-8') {
-        $return = mb_convert_encoding($return, $encoding, "UTF-8");
-    }
-
-    return $return;
 }
